05b9941f6b67bc908cd9af919748f55fbfc4a203
Derane <vsierhieiev-its24@lll.kpi.ua>
2024-02-09 23:15:05 +0200

    add edit controller

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,16 +17,51 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts, EntityManagerInterface $entityManager): Response
     {
+
         return $this->render('micro_post/index.html.twig', [
+
             'posts' => $posts->findAll(),
         ]);
     }
 
-    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    #[Route('/micro-post/{post<\d+>}', name: 'app_micro_post_show')]
     public function showOne(MicroPost $post)
     {
         return $this->render('micro_post/show.html.twig', [
             'post' => $post
+        ]);
+    }
+
+    #[Route('/micro-post/add', name: 'app_micro_post_add')]
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MicroPostType::class, new MicroPost());
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $entityManager->persist($post);
+            $entityManager->flush();
+            $this->addFlash('success', 'your micropost successfully have been added to db');
+            return $this->redirectToRoute('app_micro_post');
+        }
+        return $this->render('micro_post/add.html.twig', [
+            'form' => $form
+        ]);
+    }
+    #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    public function edit(MicroPost $post, Request $request, EntityManagerInterface $entityManager): Response
+    {
+      $form = $this->createForm(MicroPostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $entityManager->persist($post);
+            $entityManager->flush();
+            $this->addFlash('success', 'your micropost successfully have been added to db');
+            return $this->redirectToRoute('app_micro_post');
+        }
+        return $this->render('micro_post/add.html.twig', [
+            'form' => $form
         ]);
     }
 }
