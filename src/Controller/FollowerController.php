3c02f70dadd7a4e811ceb40317d3b17a727c5ba4
Derane <vsierhieiev-its24@lll.kpi.ua>
2024-02-19 00:00:26 +0200

    add follower controller

---
+++ src/Controller/FollowerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FollowerController extends AbstractController
+{
+    #[Route('/follow/{id}', name: 'app_follow')]
+    public function follow(User $user, ManagerRegistry $managerRegistry, Request $request): Response
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+        if ($currentUser !== $user) {
+            $currentUser->follow($user);
+            $managerRegistry->getManager()->flush();
+        }
+        return $this->redirect($request->headers->get('referer'));
+    }
+    #[Route('/unfollow/{id}', name: 'app_unfollow')]
+    public function unfollow(User $user, ManagerRegistry $managerRegistry, Request $request): Response
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+        if ($currentUser !== $user) {
+            $currentUser->unfollow($user);
+            $managerRegistry->getManager()->flush();
+        }
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
